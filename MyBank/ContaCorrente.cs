edaa4f6fb7f12230ede0ab5788fa22ecf1a5ada5
Thúlio Balbuena <thuliobalbuena@hotmail.com>
2021-09-15 09:38:06 -0300

    Adicionar metodos de transferencia

--- MyBank/ContaCorrente.cs
+++ MyBank/ContaCorrente.cs
@@ -1,6 +1,37 @@
 public class ContaCorrente{
-    public string titular;
-    public int agencia;
-    public int senha;
-    public double saldo;
+   public string titular;
+   public int agencia;
+   public int senha;
+   public double saldo;
+
+        public ContaCorrente(string t_titular,int a_agencia,int s_senha,double s_saldo)
+        {
+            titular = t_titular;
+            agencia = a_agencia;
+            senha = s_senha;
+            saldo = s_saldo;
+}          
+        public bool Sacar(double Valor){
+            if(this.saldo < Valor){
+                return false;
+            }
+            else{
+                this.saldo -= Valor;
+                return true;
+            }
+        }
+        public void Depositar(double Valor){
+            this.saldo += Valor;
+        }
+
+        public bool Transferir(double Valor,ContaCorrente contaDestino){
+            if(this.saldo < Valor){
+                return false;
+            }
+            else{
+                this.Sacar(Valor);
+                contaDestino.Depositar(Valor);
+                return true;
+            }
+        }
 }
--- MyBank/Program.cs
+++ MyBank/Program.cs
@@ -6,21 +6,28 @@ namespace MyBankdot
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaTui = new ContaCorrente();
-            contaTui.titular = "Thulio";
-            contaTui.agencia = 1452;
-            contaTui.senha = 113;
-            contaTui.saldo = 100.50;
-
-            ContaCorrente contaMila = new ContaCorrente();
-
-            contaMila.titular = "Mila";
-            contaMila.agencia = 532;
-            contaMila.senha = 412;
-            contaMila.saldo = 430;
+            ContaCorrente contaTui = new ContaCorrente("Thulio", 1234, 213, 142);
+            ContaCorrente contaMila = new ContaCorrente("Camila", 4234, 13, 643);
 
             Console.WriteLine("o titular é " + contaTui.titular);
             Console.WriteLine("o titular é " + contaMila.titular);
+
+            contaTui.Sacar(100);
+            contaMila.Sacar(200);
+
+            Console.WriteLine("o seu saldo é " + contaTui.saldo);
+            Console.WriteLine("o seu saldo é " + contaMila.saldo);
+
+            contaTui.Depositar(250);
+            contaMila.Depositar(350);
+
+            Console.WriteLine("o seu saldo é " + contaTui.saldo);
+            Console.WriteLine("o seu saldo é " + contaMila.saldo);
+
+            contaTui.Transferir(250, contaMila);
+
+            Console.WriteLine("o seu saldo depois da transferência é " + contaMila.saldo);
+
         }
     }
 }
